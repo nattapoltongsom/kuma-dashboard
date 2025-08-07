b0fb4087303377fbacc88bf1347db23015013f91
FFF <nattapol.tongsom@kingpower.com>
2025-08-07 12:59:47 +0700

    push

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import SummaryView from '../views/SummaryView.vue'
 
 const routes = [
@@ -35,7 +35,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory('/kuma/'), 
+  history: createWebHashHistory('/kuma/'), 
   routes
 })
 
